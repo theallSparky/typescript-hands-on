b5f13e5c8bcf2a00d736f608eb0cdbf007c52473
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-21 08:53:05 -0500

    Added some more functions as practice in sum.ts including passing objects as arguments in functions

--- src/sum.ts
+++ src/sum.ts
@@ -10,3 +10,35 @@ function sum(message: string, ...numbers: number[]): string {
 
 let result = sum("The total is: ", 1, 2, 3, 4, 5);
 console.log(result);
+
+function logMessage(message: string) {
+  console.log(message);
+}
+logMessage("Hello Typescript!"); // This type is void because there is no return, an absence of a value.
+
+function logMessageVoid(message: string): void {
+  console.log(message); // Once void has been set as type, adding a return means a data type is being returned, so they're incompatible.
+}
+
+// Using objects as parameters in functions
+function createEmployee({ id }: { id: number }): {
+  id: number;
+  isActive: boolean;
+} {
+  return { id, isActive: id % 2 === 0 };
+}
+const first = createEmployee({ id: 1 });
+const second = createEmployee({ id: 2 });
+console.log(first, second);
+
+function createStudent(student: { id: number; name: string }): void {
+  console.log(
+    `Welcome to the course ${student.name.charAt(0).toUpperCase()}` +
+      `${student.name.slice(1).toLowerCase()}!!`
+  );
+}
+const newStudent = {
+  id: 3,
+  name: "Timtim-timmehhhhh", // Obligatory South Park reference
+};
+console.log(createStudent(newStudent));
